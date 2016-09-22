36d81e44dbe49c5383b1e3da735b253e9a888074
student <student@c2025m-17.main.ad.rit.edu>
2016-09-22 17:05:23 -0400

    Finished template

--- Galaba/GameViewController.swift
+++ Galaba/GameViewController.swift
@@ -12,44 +12,94 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    // MARK - ivars -
+    var gameScene: GameScene?
+    var skView: SKView!
+    let showDebugData = true
+    let screenSize = CGSize(width: 1080, height: 1920)
+    let scaleMode = SKSceneScaleMode.aspectFill
+    
+    // MARK - Initialization -
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+        self.becomeFirstResponder()
+        skView = self.view as! SKView
+        loadHomeScene()
+        
+        // debug stuff
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = showDebugData
+        skView.showsNodeCount = showDebugData
+    }
+    
+    // MARK - Scene Management -
+    func loadHomeScene() {
+        
+        let scene = HomeScene(size: screenSize, scaleMode: scaleMode, sceneManager: self)
+        let reveal = SKTransition.crossFade(withDuration: 1)
+        skView.presentScene(scene, transition: reveal)
+    }
+    
+    func loadGameScene(levelNum: Int, totalScore: Int) {
+        
+        gameScene = GameScene(size: screenSize, scaleMode: scaleMode, levelNum: levelNum, totalScore: totalScore, sceneManager: self)
+        
+        let reveal = SKTransition.doorsOpenHorizontal(withDuration: 1)
+        skView.presentScene(gameScene!, transition: reveal)
+        
+        MotionMonitor.sharedMotionMonitor.startUpdates()
+    }
+    
+    func loadLevelFinishScene(results: LevelResults){
+        
+        gameScene = nil
+        let scene = LevelFinishScene(size: screenSize, scaleMode: scaleMode, results: results, sceneManager: self)
+        let reveal = SKTransition.crossFade(withDuration: 1)
+        skView.presentScene(scene, transition: reveal)
+        
+        MotionMonitor.sharedMotionMonitor.stopUpdates()
+    }
+    
+    func loadGameOverScene(results: LevelResults){
+        
+        gameScene = nil
+        let scene = GameOverScene(size: screenSize, scaleMode: scaleMode, results: results, sceneManager: self)
+        let reveal = SKTransition.crossFade(withDuration: 1)
+        skView.presentScene(scene, transition: reveal)
+    }
+    
+    // MARK - Motion Events -
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if motion == .motionShake {
             
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+            print("detected shake")
+            gameScene?.makeSprites(howMany: 10)
         }
     }
-
+    
+    // MARK - Lifecycle -
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        // release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
+        
+        return true
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        
         return true
     }
 }
